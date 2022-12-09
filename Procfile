eff1b135358f81665961e7cc697340f320fd7a6d
lillie-ann <lah263@byu.edu>
2022-12-09 10:38:59 -0700

    railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && gunicorn 403project.wsgi
--- pickleballsite/settings.py
+++ pickleballsite/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^#$yzxz%_r6gb6my=n^5nhxebtcd#7&cya)n(g$qefrc6i@ct6'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
